4739425aca0da490e74c29b613cde4a17ef938e1
johnazt <john.azt.dev@gmail.com>
2022-08-17 22:51:45 -0400

    Add

--- assets/js/data/db.js
+++ assets/js/data/db.js
@@ -3,7 +3,7 @@ const items = [
       id: 1,
       name: 'Hoodies',
       price: 14.00,
-      image: '/assets/images/featured1.png',
+      image: 'assets/images/featured1.png',
       category: 'hoodies',
       quantity: 10
     },
@@ -11,7 +11,7 @@ const items = [
       id: 2,
       name: 'Shirts',
       price: 24.00,
-      image: '/assets/images/featured2.png',
+      image: 'assets/images/featured2.png',
       category: 'shirts',
       quantity: 15
     },
@@ -19,7 +19,7 @@ const items = [
       id: 3,
       name: 'Sweatshirts',
       price: 24.00,
-      image: '/assets/images/featured3.png',
+      image: 'assets/images/featured3.png',
       category: 'sweatshirts',
       quantity: 20
     }
